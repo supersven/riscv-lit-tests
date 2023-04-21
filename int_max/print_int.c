defce186ec0ea521d0561db13af2ef01e7f8060d
Sven Tennie <sven.tennie@wire.com>
2023-04-21 07:29:18 +0000

    Add int_min and int_max tests

---
+++ int_max/print_int.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Rts.h"
+
+RTS_FUN_DECL(test);
+
+StgRegTable * StgRun (StgFunPtr f, StgRegTable *basereg);
+
+// Call cmm and print result regs
+int main(int argc, char *argv[]) {
+    StgRegTable * regTable = malloc(sizeof(StgRegTable));
+
+    StgInt result = (StgWord) StgRun((StgFunPtr) test, regTable);
+
+    printf("result: %ld\n", result);
+    return 0;
+}
